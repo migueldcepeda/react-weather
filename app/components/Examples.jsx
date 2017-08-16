ff1a5f589391738de3dec7e1bf059f7345e6d881
Miguel Cepeda <miguel@MacBook-Pro.local>
2017-08-16 15:02:19 -0400

    Updates copy

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -11,7 +11,7 @@ var React = require('react');
 //presentational component >> sateless functional component
 var Examples = (props) => {
   return (
-    <h3>Examples Component</h3>
+    <h3>Examples Component!</h3>
   )
 };
 
